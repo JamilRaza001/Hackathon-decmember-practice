e4c65a6a08b7418b7483809f5f78b12eb078ae0c
[Muhammad Jamil Raza Attari] <jamilraza001@gmail.com>
2024-12-05 02:40:56 +0500

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+const TopHeader = () => {
+  return (
+    <div className="w-[1280px] h-[48px] bg-[#000000] text-[#FAFAFA] flex">
+      <p className="">
+        Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+        <span>ShopNow</span>
+      </p>
+      <select name="select" id="select">
+        <option value="">English</option>
+        <option value="">English</option>   
+      </select>
+    </div>
+  );
+};
+
+export default TopHeader;
+
+export const Header = () => {
+  return <div></div>;
+};
